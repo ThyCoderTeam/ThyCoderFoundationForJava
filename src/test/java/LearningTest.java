4288fd646cf6dd2605e2e7a0c1489339dbfa5ca7
zj05409 <zj05409@qq.com>
2014-07-28 17:37:19 +0800

    second commit

--- src/test/java/LearningTest.java
+++ src/test/java/LearningTest.java
@@ -1,26 +1,35 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by zhangjie on 14-7-28.
  */
 public class LearningTest {
+    @Test
+    public void testDeclaredMethods() {
+        System.out.println("A" + Arrays.toString(A.class.getDeclaredMethods()));
+        System.out.println("A1" + Arrays.toString(A1.class.getDeclaredMethods()));
+        System.out.println("A11" + Arrays.toString(A11.class.getDeclaredMethods()));
+    }
     public static interface A {
         public void hello();
     }
-    public static class A1 implements A{
+
+    public static class A1 implements A {
         @Override
         public void hello() {
 
         }
     }
-    public static class A11 extends A1{
+
+    public static class A11 extends A1 {
         @Override
         public void hello() {
 
         }
     }
-    @Test
-    public void testDeclaredMethods(){
 
+    public static class A12 extends A1 {
     }
 }
